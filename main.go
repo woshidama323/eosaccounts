a73f35fd3cda97a1f28fe3ee2962e33d9cb0fda9
MK <mk@MKdeMacBook-Pro.local>
2020-01-03 18:13:12 +0800

    no use

--- main.go
+++ main.go
@@ -7,17 +7,16 @@ import (
 	"net/url"
 	"time"
 
-	"github.com/gorilla/websocket"
-
-	// "regexp"
 	"flag"
 	"os"
 	"strconv"
 
 	"github.com/go-redis/redis"
-	"golang.org/x/net/html"
 	"strings"
-)
+
+	"golang.org/x/net/html"
+	"github.com/gorilla/websocket"
+	"strings"
 
 var (
 	Info    *log.Logger
@@ -49,205 +48,26 @@ func main() {
 	rediskey := flag.String("rediskey", "forloopsend", "a key for storing the msg to redis")
 
 	flag.Parse()
-	u := url.URL{Scheme: "wss", Host: "api-v1.eosflare.io", Path: "/socket.io/"}
+
+	u := url.URL{Scheme: "https", Host: "api.newdex.vip", Path: "/v1/candles"}
 	v := url.Values{}
-	v.Add("EIO", "3")
-	v.Add("transport", "websocket")
+
+	//https://api.newdex.vip/v1/candles?symbol=defindinvest-defi-eos&time_frame=1day&size=100
+	v.Add("symbol", "defindinvest-defi-eos")
+	v.Add("time_frame", "1day")
+	v.Add("size",100)
 
 	urlstr := u.String() + "?" + v.Encode()
 
-	he := http.Header{}
-	he.Add("origin", "https://eosflare.io")
-	c, _, err := websocket.DefaultDialer.Dial(urlstr, he)
+	resp, err := http.Get(urlstr)
 	if err != nil {
 		//链接失败blockchain
-		Info.Println("......", err)
+		Error.Println("......", err)
 		return
 	}
 
-	defer c.Close()
+	defer resp.Body.Close()
 
-	tc2 := time.NewTicker(time.Second * 3)
-	go func() {
-		i := *start
-		Info.Println(".....start....", i)
-		for {
-			select {
-
-			case t := <-tc2.C:
-				Info.Println("=====", t)
-
-				// ["message","{\"_url\":\"/chain/get_user_whales\",\"_method\":\"POST\",\"_headers\":{\"content-type\":\"application/json\"},\"page\":1,\"limit\":500,\"sortBy\":\"total\",\"ascending\":false,\"lang\":\"zh-CN\"}"]
-
-				jsonstr := get_user_whales + strconv.FormatInt(int64(i), 10) + "," + "\\\"account\\\":\\\"" + *con + "\\\"" + tokenurl
-
-				reqstr := "42" + "[\"message\",\"" + jsonstr + "\"]"
-
-				Info.Println("final.request..", reqstr)
-				err = c.WriteMessage(websocket.TextMessage, []byte(reqstr))
-
-				if err != nil {
-					Info.Println("..........**,.....", err)
-					time.Sleep(1)
-					continue
-				}
-
-			}
-
-			i++
-			Info.Println(".....monitor....", i)
-			Info.Println(".....compare stop....", *stop)
-			if i > *stop {
-				Info.Println("have gotten the 25w users.")
-				//break
-				return
-			}
-			// break
-		}
-	}()
-
-	// go func(){
-
-	// 	// ["message","{\"_url\":\"/chain/get_user_whales\",\"_method\":\"POST\",\"_headers\":{\"content-type\":\"application/json\"},\"page\":1,\"limit\":500,\"sortBy\":\"total\",\"ascending\":false,\"lang\":\"zh-CN\"}"]
-	// 	c.WriteMessage(websocket.TextMessage,)
-	// }()
-
-	for {
-		_, msg, err := c.ReadMessage()
-		if err != nil {
-			Info.Println("failed to get the message..", err)
-			return
-			//如果链接中断，重新链接
-			c, _, err = websocket.DefaultDialer.Dial(urlstr, he)
-			if err != nil {
-				//链接失败blockchain
-				Info.Println("......", err)
-				time.Sleep(1)
-				//希望运行完可以结束掉，这里直接return
-				continue
-			}
-
-		}
-		Info.Println(".....++++++++", string(msg))
-
-		var out interface{}
-
-		if len(msg) < 2 {
-			continue
-		}
-
-		if string(msg[:1]) == "0" {
-			Info.Println("ping pong msg:", string(msg))
-			continue
-		}
-
-		if string(msg[:2]) == "42" {
-			Info.Println("accounts msg")
-			err := json.Unmarshal(msg[2:], &out)
-			if err != nil {
-				Info.Println("failed to unmarshal the bytes", err)
-			}
-		} else {
-			continue
-		}
-
-		switch m := out.(type) {
-		case map[string]interface{}:
-			// Info.Println("....",m)
-
-			if v, ok := out.(map[string]interface{})["sid"]; ok {
-				Info.Println("start msg", v)
-			}
-
-		// if v2,ok2 := out.(map[string]interface{})["sid"];ok2{
-
-		// 	Info.Println("start msg")
-		// }
-		case []interface{}:
-			// Info.Println("______",out)
-			go func(input interface{}) {
-				//
-				rc := redis.NewClient(&redis.Options{
-					Addr:     "localhost:6379",
-					Password: "", // no password set
-					DB:       14, //14,  // use default DB
-				})
-
-				getholder := input.([]interface{})[1].(string)
-				// Info.Println(getholder)
-
-				var test interface{}
-				json.Unmarshal([]byte(getholder), &test)
-				if test.(map[string]interface{})["holders"] == nil {
-					// Info.Println("something wrong with it ..", test)
-					rcp := rc.Pipeline()
-					for k, v := range test.(map[string]interface{}) {
-						Info.Println("k:", k, "~~v:", v)
-
-						if k == "actions" {
-							//建立pipe
-							
-							for kk, vv := range v.([]interface{}) {
-								Info.Println("kk:", kk, "~~vv:", vv.(map[string]interface{})["info"])
-								if vv.(map[string]interface{})["type"] != "Sent" {
-                                    continue
-								}
-								doc,err := html.Parse(strings.NewReader(vv.(map[string]interface{})["info"].(string)))
-								if err != nil {
-									Info.Println("failed to get doc")
-								}
-								var f func(*html.Node)
-                                f = func(n *html.Node) {
-                                    if n.Type == html.ElementNode && n.Data == "a" {
-                                        for _, a := range n.Attr {
-                                            if a.Key == "href" {
-												Info.Println(strings.Split(a.Val,"/")[2])
-												rcp.SAdd(*rediskey,strings.Split(a.Val,"/")[2])
-                                                break
-                                            }
-                                        }
-                                    }
-                                    for c := n.FirstChild; c != nil; c = c.NextSibling {
-                                        f(c)
-                                    }
-                                }
-                                f(doc)
-							}
-						}
-
-					}
-					_, err := rcp.Exec()
-					if err !=nil {
-						panic(err)
-					}
-					return
-				}
-				// for _, x := range test.(map[string]interface{})["holders"].([]interface{}) {
-
-				// 	liquid := 1.0
-				// 	if *con != "" {
-				// 		liquid = x.(map[string]interface{})["balance"].(float64)
-				// 	} else {
-				// 		liquid = x.(map[string]interface{})["liquidity"].(float64)
-				// 	}
-
-				// 	if liquid < *minasset {
-				// 		Info.Println("it's the small account: ", x)
-				// 		continue
-				// 	}
-				// 	storestr := x.(map[string]interface{})["owner"].(string) + "_" + strconv.FormatFloat(liquid, 'f', -1, 64)
-				// 	Info.Println("....._+_+_+", storestr)
-				// 	err = rc.SAdd(*rediskey, storestr).Err()
-				// 	if err != nil {
-				// 		Info.Println("get errors ??...", err)
-				// 		panic(err)
-				// 	}
-				// }
-
-			}(out)
-
-		default:
-			Info.Println("Unsupported message", m)
-		}
-	}
+	
+	
 }
